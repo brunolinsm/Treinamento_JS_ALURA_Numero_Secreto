5ac609560ef6144ea6bf9524ee96b6868b0bee9b
Bruno Lins Maia <brunolinsm@gmail.com>
2024-02-08 17:23:42 -0300

    Final da lição

--- validacao.js
+++ validacao.js
@@ -14,7 +14,9 @@ function verificaValidadeChute(chute) {
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
         <h2>Você acertou! Parabéns!</h2>
-        <h3>O número secreto era ${numeroSecreto}</h3>`
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
     }
 
     else if (numero > numeroSecreto) {
@@ -35,3 +37,9 @@ function chuteForInvalido(numero) {
 function numeroForaFaixaValores(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', evento => {
+    if (evento.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
